558cd87d70bb6d0d85548acfdc354ff184e22664
Daniel Moreno <123407190+danmoree@users.noreply.github.com>
2026-08-14 23:07:54 -0600

    Initial Commit

---
+++ Lilypad/LilypadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilypadApp.swift
+//  Lilypad
+//
+//  Created by Daniel Moreno on 8/14/26.
+//
+
+import SwiftUI
+
+@main
+struct LilypadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
